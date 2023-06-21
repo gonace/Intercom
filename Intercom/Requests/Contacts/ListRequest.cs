423d50de41e26458b02235245efad75a478d823a
Erik Hennerfors <erik.hennerfors@obscured.se>
2023-06-21 19:43:51 +0200

    Update(s)

--- Intercom/Requests/Contacts/ListRequest.cs
+++ Intercom/Requests/Contacts/ListRequest.cs
@@ -1,10 +1,9 @@
 ﻿using Intercom.Abstractions;
-using Intercom.Extensions;
 
 namespace Intercom.Requests.Contacts
 {
     public class ListRequest : PlainRequest
     {
-        public override string Uri => $"contacts{this.ToQueryString()}";
+        public override string Uri => "contacts";
     }
 }
